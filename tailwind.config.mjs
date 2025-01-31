c112e2ead708d54188635a0f67a12be832f87dbe
ahtisham02 <ahtisham02>
2025-01-31 11:57:05 +0500

    chnges

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,9 +16,12 @@ export default {
         textclr: "var(--textclr)",
       },
       fontFamily: {
-        'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+        "plus-jakarta": ['"Plus Jakarta Sans"', "sans-serif"],
       },
     },
   },
   plugins: [],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
