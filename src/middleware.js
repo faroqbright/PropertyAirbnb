bed277fcfd3cab25abe796edd25bf07abc5637fe
ahtisham02 <ahtisham02>
2025-02-24 12:13:25 +0500

    middle ware setup

--- src/middleware.js
+++ src/middleware.js
@@ -3,18 +3,21 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const { pathname } = request.nextUrl;
   const authPages = ["/Auth/Login", "/Auth/Signup", "/Auth/Forgot", "/Auth/Personal", "/Auth/ResetPassword"];
-
-  const isAuthPage = authPages.some((page) => pathname.startsWith(page));
+  const protectedPages = ["/Landing/Profile"];
 
   const uid = request.cookies.get("uid")?.value;
 
-  if (uid && isAuthPage) {
+  if (uid && authPages.some((page) => pathname.startsWith(page))) {
     return NextResponse.redirect(new URL("/Landing/Home", request.url));
+  }
+
+  if (!uid && protectedPages.some((page) => pathname.startsWith(page))) {
+    return NextResponse.redirect(new URL("/Auth/Login", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/Auth/:path*", "/Landing/Home"],
+  matcher: ["/Auth/:path*", "/Landing/Home", "/Landing/Profile"],
 };
